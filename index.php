a79ccacb4f92c45d26a15c2324d43540d6d46a4f
root <ngomfa@gmail.com>
2018-11-16 15:56:12 +0100

    exo8

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie1_Exo8</title>
+</head>
+<body>
+    <?php
+    //1-Créer trois variables.
+    //2-Dans la première mettre le résultat de l'opération 3 + 4.
+    //3-Dans la deuxième mettre le résultat de l'opération 5 * 20.
+    //4-Dans la troisième mettre le résultat de l'opération 45 / 5.
+    //5-Afficher le contenu des variables.
+    $addition = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
+      echo $addition . " " . $multiplication . " " . $division;
+  ?>
+</body>
+</html>
